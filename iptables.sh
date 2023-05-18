b389723bce9c545783127ec75d008742e05e862e
Rake <sayapov.r@gmail.com>
2023-05-18 17:03:18 +0600

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -2,8 +2,7 @@
 IPT=/usr/sbin/iptables
 
 POLICY=DROP
-LAN=ens224
-WAN=ens192
+LAN=ens192
 ################x####################
 $IPT -F INPUT
 $IPT -F
@@ -20,22 +19,16 @@ $IPT -P OUTPUT $POLICY
 $IPT -A INPUT -i lo -j ACCEPT
 $IPT -A OUTPUT -o lo -j ACCEPT
 
-#$IPT -A INPUT -i $LAN -j ACCEPT
-#$IPT -A OUTPUT -o $LAN -j ACCEPT
-
-#$IPT -A INPUT -i $WAN -j ACCEPT
-#$IPT -A OUTPUT -o $WAN -j ACCEPT
-
-
+$IPT -A INPUT -i $LAN -j ACCEPT
+$IPT -A OUTPUT -o $LAN -j ACCEPT
 
 $IPT -I INPUT -i $WAN -m state --state ESTABLISHED,RELATED -j ACCEPT
-$IPT -I INPUT -p tcp -s 89.218.153.38  -j ACCEPT
+#$IPT -I INPUT -p tcp -s 89.218.153.38  -j ACCEPT
 
 $IPT -A INPUT -j $POLICY
 
-
 $IPT -I OUTPUT -o $WAN -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 ############### DEBUG #############
-$IPT -I INPUT -i $WAN -p tcp --dport 22 ! -s 89.218.153.38 -j LOG  --log-prefix "Iptables: SSH 22 ATЕEMPT"
+#$IPT -I INPUT -i $WAN -p tcp --dport 22 ! -s 89.218.153.38 -j LOG  --log-prefix "Iptables: SSH 22 ATЕEMPT"
 
 $IPT -L -v
